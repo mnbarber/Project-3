2a13f6a28d5893234b066a1d4cff0f96a6510304
mnbarber <mnbarber1993@gmail.com>
2022-05-18 11:07:36 -0700

    still struggling with second endpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,8 +12,7 @@ function App() {
   const [character, setCharacter] = useState('');
   const [realm, setRealm] = useState('');
   const [achievements, setAchievements] = useState([]);
-  const [acData, setAcData] = useState({});
-  const [id, setID] = useState('');
+  
 
   const findCharacter = async () => {
     try {
@@ -39,18 +38,6 @@ function App() {
         if(apiData) {
             setData(apiData);
         };
-
-        const achievementID = await fetch(`https://us.api.blizzard.com/data/wow/achievement/${id}?namespace=static-us&locale=en_US`, {
-          headers: {
-            Authorization: `Bearer ${tokenBearer}`,
-            'content-type': 'application/x-www-form-urlencoded'
-          },
-          method: 'GET'
-        });
-        const acIdData = await achievementID.json();
-        if(acIdData) {
-          setAcData(acIdData);
-        }
     } catch(error) {
         console.log(error);
     };
@@ -60,9 +47,9 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
-        <Route path='/' element={<Search data={data} findCharacter={findCharacter} setCharacter={setCharacter} setRealm={setRealm} character={character} realm={realm} achievements={achievements} setAchievements={setAchievements} setID={setID} />} />
-        <Route path='/achievementslist' element={<AchievementList data={data} character={character} achievements={achievements} id={id} setID={setID} />} />
-        <Route path='/achievementslist/:id' element={<Achievement acData={acData} setAcData={setAcData} />} />
+        <Route path='/' element={<Search data={data} findCharacter={findCharacter} setCharacter={setCharacter} setRealm={setRealm} character={character} realm={realm} achievements={achievements} setAchievements={setAchievements} />} />
+        <Route path='/achievementslist' element={<AchievementList data={data} character={character} achievements={achievements} />} />
+        <Route path='/achievementslist/:id' element={<Achievement data={data} achievements={achievements} />} />
       </Routes>
     </div>
   );
--- frontend/src/pages/Achievement.js
+++ frontend/src/pages/Achievement.js
@@ -3,31 +3,51 @@ import React, { useState } from "react";
 
 function Achievement(props) {
     const [achievementData, setAchievementData] = useState({});
+    const achievementID = props.data.achievements.id;
+    console.log(achievementData);
+
+    
 
     const getAchievementInfo = async () => {
         try {
             const getToken = await fetch('https://us.battle.net/oauth/token', {
-            body: 'grant_type=client_credentials',
-            headers: {
-                Authorization: 'Basic N2ZiYWFlN2ExYWNjNGM0OWE1NmM2YmJhYmQwNTA3ZWI6UGhzNERkTnZmanY3dno1ZUxsYjFDcFFsa1Bvampqalo=', 'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            method: 'POST'
-        });
-        const tokenData = await getToken.json();
-        const tokenBearer = tokenData.access_token;
-        console.log(tokenBearer);
-        const getAchievement = await fetch(`https://us.api.blizzard.com/data/wow/achievement/${id}?namespace=static-us&locale=en_US`, {
-          headers: {
-            Authorization: `Bearer ${tokenBearer}`,
-            'content-type': 'application/x-www-form-urlencoded'
-          },
-          method: 'GET'
-        });
+                body: 'grant_type=client_credentials',
+                headers: {
+                    Authorization: 'Basic N2ZiYWFlN2ExYWNjNGM0OWE1NmM2YmJhYmQwNTA3ZWI6UGhzNERkTnZmanY3dno1ZUxsYjFDcFFsa1Bvampqalo=', 'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                method: 'POST'
+            });
+            const tokenData = await getToken.json();
+            const tokenBearer = tokenData.access_token;
+            console.log(tokenBearer);
+            const getAchievement = await fetch(`https://us.api.blizzard.com/data/wow/achievement/${achievementID}?namespace=static-us&locale=en_US`, {
+                headers: {
+                    Authorization: `Bearer ${tokenBearer}`,
+                    'content-type': 'application/x-www-form-urlencoded'
+                },
+                method: 'GET'
+            });
+            const acData = await getAchievement.json();
+            console.log(acData)
+            if(acData) {
+                setAchievementData(acData);
+            }
+        } catch(error) {
+            console.log(error);
         }
     }
+    if(achievementID) {
+        getAchievementInfo();
+    }
+
     return (
         <div>
-            <h2>no</h2>
+            <h2>{achievementData.name}</h2>
+            <hr></hr>
+            <ul>
+                <li>{achievementData.description}</li>
+                {/* <li><img src={achievementData.media.key.href} alt='achievement' /></li> */}
+            </ul>
         </div>
     );
 };
